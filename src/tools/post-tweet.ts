baac1fe264674083a1e8453d68ebb31886caf5d9
Khaja Mohiddin Shaik <khajaphysist@gmail.com>
2026-03-17 16:46:38 +0530

    fix post tweet api

--- src/tools/post-tweet.ts
+++ src/tools/post-tweet.ts
@@ -6,8 +6,12 @@ export default defineTool({
   description: "Post a tweet to X/Twitter",
   schema: {
     text: z.string().describe("The text content of the tweet (max 280 characters)"),
+    reply_to: z.string().optional().describe("Tweet ID to reply to"),
   },
   handler: async (client, _userId, args) => {
-    return client.posts.create({ text: args.text });
+    return client.posts.create({
+      text: args.text,
+      ...(args.reply_to && { reply: { in_reply_to_tweet_id: args.reply_to } }),
+    });
   },
 });
